2023b81db5857958cdad9115a3080b82c9f00d5f
Darylcxz <dcxz96@live.com>
2016-01-30 04:55:10 +0800

    Items pick up script working with placeholders and lots of debug.log

---
+++ Assets/Scripts/ItemsManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemsManager : MonoBehaviour {
+    private int ingredientlevel = 0;
+    private int ingredientindex;
+    private int itemnumber;
+
+    [SerializeField] GameObject[] items;
+
+    void Start()
+    {
+        ingredientindex = 0;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Ingredient"))
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                int.TryParse(other.gameObject.name, out itemnumber);
+                if(itemnumber == ingredientindex)
+                {
+                    if(itemnumber == ingredientlevel)
+                    {
+                        Debug.Log("Light Shines Brighter");
+                        ingredientlevel++;
+                        ingredientindex = 0;
+                        SpawnNewItems();
+                    }
+                    Debug.Log("Correct item " + itemnumber);
+                    ingredientindex++;
+                }
+
+                else
+                {
+                    Debug.Log("Wrong item " + itemnumber + " this should be " + ingredientindex);
+                }
+            }
+        }
+    }
+
+    void SpawnNewItems()
+    {
+        
+    }
+}
