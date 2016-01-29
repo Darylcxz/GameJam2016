ba7fb43724d41eff4c2cca22f9be60aedf3efab7
Amadeus Chia <foodpunch@gmail.com>
2016-01-29 20:54:21 +0800

    Changed Settings to 2D Unity VS shit Made the screen fade script

---
+++ Assets/Scripts/ScreenTransition.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScreenTransition : MonoBehaviour {
+	//fade in black, fade out black
+	[SerializeField]
+	Image fadeUI;		//UI for fade
+
+	float _t; //update time keeper
+	bool bFade;
+	float _alpha;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F)&&!bFade)
+		{
+			StartCoroutine("Faded");
+			bFade = true;
+		}
+		fadeUI.color = new Color(fadeUI.color.r, fadeUI.color.g, fadeUI.color.b, _alpha);
+	}
+	IEnumerator Faded()
+	{
+		for (float i = 0; i < 1; i+=Time.deltaTime*3.5f)
+		{
+			Debug.Log(i);
+			_alpha = i;
+			yield return null;
+		}
+		_alpha = 1;
+		yield return new WaitForSeconds(2);
+		for (float j = 1; j > 0; j-=Time.deltaTime)
+		{
+			Debug.Log(j);
+			_alpha = j;
+			yield return null;
+		}
+		_alpha = 0;
+		bFade = false;
+			
+	}
+}
