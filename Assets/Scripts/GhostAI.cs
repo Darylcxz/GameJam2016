d2c31fc7c9ab9ae86470b802c052d24346d905da
Amadeus Chia <foodpunch@gmail.com>
2016-01-30 09:46:56 +0800

    Added ghost AI Need to fix sprite rotation Added fade effect on AI

---
+++ Assets/Scripts/GhostAI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostAI : MonoBehaviour {
+
+	Rigidbody2D _rb;  //rigidbody of boo
+	Transform target; //player target
+	Vector3 direction;//direction to face
+
+	SpriteRenderer booSprite;
+
+	[SerializeField]
+	float speed = 5f;
+	[SerializeField]
+	float steerForce;
+	[SerializeField]
+	float rotateSpeed;
+	[SerializeField]
+	float translateSpeed;
+	// Use this for initialization
+	void Start () {
+		_rb = GetComponent<Rigidbody2D>();
+		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+		booSprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		direction = (target.position - transform.position).normalized;
+		direction.z = 0;
+		
+		//Vector3 vectorToTarget = targetTransform.position - transform.position;
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+		transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime *rotateSpeed);
+		transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+		transform.position += transform.right * Time.deltaTime * translateSpeed;
+
+		booSprite.color = new Color(1,1,1,(Mathf.PingPong(Time.time / 2, 1 - 0)));
+		
+		
+
+		if (transform.eulerAngles.z > 90 && transform.eulerAngles.z < 360)
+		{
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+		else
+		{
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+
+		
+	
+	}
+}
---
+++ Assets/Scripts/SmoothCamFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothCamFollow : MonoBehaviour {
+
+	 public float dampTime = 0.25f;
+     private Vector3 velocity = Vector3.zero;
+     public Transform target;
+	 [SerializeField]
+	 Camera mainCam;
+	 void Start()
+	 {
+		 mainCam = mainCam.GetComponent<Camera>();
+	 }
+     void Update () 
+     {
+		 Vector3 point = mainCam.WorldToViewportPoint(target.position);
+		 Debug.Log(point);
+		 //clamp at 0.1 and 0.4 maybe 0.2 and 0.5?
+		 
+		 
+		 //if (point.x > 0)
+		 //{
+             
+		 //	Vector3 delta = target.position - mainCam.ViewportToWorldPoint(new Vector3(0.3f, 0.4f, point.z));
+		 //	Vector3 destination = transform.position + delta;
+		 //	transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		 //}
+     }
+}
