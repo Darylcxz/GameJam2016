ba206fd5c68172ac567bd8b9bd1b87187964c91c
Amadeus Chia <foodpunch@gmail.com>
2016-01-30 23:25:44 +0800

    Added Level 1 and Level 2 Added physics material (frictionless) Fixed the colliders not sticking Added Object pooler script Added Spawn Manager script Added Game Manager Script

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectPooler : MonoBehaviour {
+	public static ObjectPooler current;
+	public GameObject _Ghosts;
+
+	public int pooledAmount = 5;
+	public bool canGrow;
+
+	public List<GameObject> ghostPool;
+
+	void Awake()
+	{
+		current = this;
+	}
+
+	void Start()
+	{
+		ghostPool = new List<GameObject>();
+		for (int i = 0; i < pooledAmount; i++)
+		{
+			GameObject ghostClone = (GameObject)Instantiate(_Ghosts);
+			ghostClone.SetActive(false);
+			ghostPool.Add(ghostClone);
+		}
+	}
+
+	public GameObject SpawnGhost()
+	{
+		for (int i = 0; i < ghostPool.Count; i++)
+		{
+			if (!ghostPool[i].activeInHierarchy)
+			{
+				return ghostPool[i];
+			}
+		}
+		if (canGrow)
+		{
+			GameObject ghostClone = (GameObject)Instantiate(_Ghosts);
+			ghostPool.Add(ghostClone);
+			return ghostClone;
+		}
+		return null;
+	}
+}
