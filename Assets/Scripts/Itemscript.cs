39511bd15e0f507fcb73fc62705ae97abe98adab
Darylcxz <dcxz96@live.com>
2016-01-31 03:44:30 +0800

    finished up items pickup and made items prefab

---
+++ Assets/Scripts/Itemscript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Itemscript : MonoBehaviour {
+    ParticleSystem pickupvfx;
+    SpriteRenderer item;
+	// Use this for initialization
+	void Start () {
+
+        pickupvfx = gameObject.GetComponentInChildren<ParticleSystem>();
+        item = gameObject.GetComponent<SpriteRenderer>();
+	}
+
+
+    void Collect()
+    {
+        pickupvfx.Play();
+        item.color = new Color(item.color.r, item.color.g, item.color.b, 0.0f);
+        Invoke("Destroyme", 1.0f);
+    }
+
+    void Destroyme()
+    {
+        gameObject.SetActive(false);
+    }
+}
